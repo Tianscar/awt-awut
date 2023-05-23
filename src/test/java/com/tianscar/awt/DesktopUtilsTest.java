2b9ce42ff66827949db2b5a66f9be11380249c2c
Karstian Lee <tianscar@protonmail.com>
2023-05-24 01:27:58 +0800

    Update DesktopUtilsTest.java

--- src/test/java/com/tianscar/awt/DesktopUtilsTest.java
+++ src/test/java/com/tianscar/awt/DesktopUtilsTest.java
@@ -1,13 +1,14 @@
 package com.tianscar.awt;
 
 import java.io.File;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 public class DesktopUtilsTest {
 
     public static void main(String[] args) throws URISyntaxException {
-        //DesktopUtils.browse(new URI("https://github.com"));
+        DesktopUtils.browse(new URI("https://github.com"));
         DesktopUtils.open(new File("LICENSE"));
-        //DesktopUtils.mail(new URI("mailto://tianscar@protonmail.com"));
+        DesktopUtils.mail(new URI("mailto://tianscar@protonmail.com"));
     }
 }
